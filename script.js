58516abb555d987168a6049d6f8ebd104fdbcc25
Timothy Jhey Sarmiento <timothyjhey@gmail.com>
2021-10-25 11:18:06 +0800

    Manipulating DOM with javascript

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
 let apiQuotes = [];
 
 //  Show new quotes
 const newQuote = () => {
   // Pick a ramdom quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+
+  // Check if author field is blank and replace it with "Anonymous"
+  if (!quote.author) {
+    authorText.textContent = "Anonymous";
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  //   Check quote length to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+
+  quoteText.textContent = quote.text;
 };
 
 // Get quotes from API
@@ -16,6 +37,16 @@ const getQuotes = async () => {
     newQuote();
   } catch (error) {}
 };
+
+// Tweet quote
+const tweetQuote = () => {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  open(twitterUrl, "_blank");
+};
+
+// Event listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
 // on load
 getQuotes();
