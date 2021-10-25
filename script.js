c052da5ef2ef8c49fc02171649623e8ee3b405e7
Timothy Jhey Sarmiento <timothyjhey@gmail.com>
2021-10-25 11:29:36 +0800

    Creating a loader with javascript

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
+// Show loading
+const loading = () => {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+};
+
+// Hide loading
+const complete = () => {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+};
+
 //  Show new quotes
 const newQuote = () => {
+  loading();
   // Pick a ramdom quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
@@ -24,12 +38,14 @@ const newQuote = () => {
   } else {
     quoteText.classList.remove("long-quote");
   }
-
+  //   Set quote, hide loader
   quoteText.textContent = quote.text;
+  complete();
 };
 
 // Get quotes from API
 const getQuotes = async () => {
+  loading();
   const apiUrl = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiUrl);
